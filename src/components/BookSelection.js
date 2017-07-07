ac1b07281b6fe61b5725b9e956bacda3ba317f3d
ipatch <chris.r.jones.1983@gmail.com>
2017-07-07 18:36:39 -0500

    create the `BookSelection` component

---
+++ src/components/BookSelection.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class BookSelection extends Component {
+  state = {
+
+  }
+
+  selectionTest() {
+    console.log("Hello from selectionTest method / function");
+  }
+
+  render() {
+    return(
+      <div className="book-shelf-changer">
+        <select onChange={this.selectionTest}>
+          <option value="none" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    )
+  }
+}
+
+export default BookSelection
